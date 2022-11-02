450d1dc41e89a74a70b2f8183a80531d1afb4c60
Sam Robinson <110217039+SamRobinson-2511@users.noreply.github.com>
2022-11-02 11:34:16 -0500

    commit tree 69863ca78072ca8c2d9247b14ee6a8c885e5147d

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your code here!
+// const element = document.createElement('div')
+// document.body.append(element)
+// const ul = document.createElement('ul')
+// for(let i = 0; i < 3; i++){
+//     const li = document.createElement('li')
+//     li.textContent = (i + 1).toString()
+//     ul.append(li)
+// }
+// element.append(ul);
+
+// // const main = document.getElementById('main');
+// // main.innerHTML = 
+// // "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
+
+// // const style = document.getElementById('main');
+// // style.style.height = '300px';
+// // style.style.backgroundColor = "#27647B";
+
+// // style.textContent = "You've changed what's on the screen"
+// // style.style.fontSize = '24px'
+// // style.style.marginLeft = '30px'
+// // style.style.lineHeight = 2
+
+// // element.className = 'pet-listing dog'
+// // element.classList.remove('dog');
+// // element.classList.add('cat', 'sale')
+
+// const ul2 = document.getElementsByTagName('ul')[0]
+// const secondChild = ul.querySelector('li:nth-child(2)')
+// ul2.removeChild(secondChild)
+// ul2.remove();
+
+
+document.body.remove('main')
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = 'Sam is the champion'
